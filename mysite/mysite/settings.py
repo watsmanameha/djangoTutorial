6a4166ea6b17b0b5043def56489c9ee2f5a27f0e
Eugene <62775429+watsmanameha@users.noreply.github.com>
2022-04-22 20:36:25 +0300

    part 2 (#1)

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,6 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # MY APPS
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
